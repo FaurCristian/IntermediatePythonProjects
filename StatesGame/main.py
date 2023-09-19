ebede5466047fd3b82b009adce9d4c16eab32b49
FaurCristian <faur.cristian96@yahoo.ro>
2023-09-19 16:28:15 +0300

    first commit

--- StatesGame/main.py
+++ StatesGame/main.py
@@ -17,13 +17,9 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct ",
                                     prompt="What's another state's name").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in states_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states to learn.csv")
-
         break
     if answer_state in states_list:
         guessed_states.append(answer_state)
@@ -33,6 +29,4 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-
-
-screen.mainloop()
+screen.exitonclick()
